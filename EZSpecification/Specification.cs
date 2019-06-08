1c5a02d2c8b124c1011d861cbd63c488c01ea73f
dillon <28228453+DillonAd@users.noreply.github.com>
2019-06-07 22:43:08 -0500

    Swapping ISpecification interface for abstract class to allow for easy conversions

---
+++ EZSpecification/Specification.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq.Expressions;
+
+namespace EZSpecification
+{
+    public abstract class Specification<TEntity>
+        where TEntity : IEntity
+    {
+        public abstract Expression<Func<TEntity, bool>> Expression { get; set; }
+
+        public static implicit operator Func<TEntity, bool>(Specification<TEntity> specification) =>
+            specification.Expression.Compile();
+    }
+}

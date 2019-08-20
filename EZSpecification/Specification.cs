b9f13fb7f33be82382938fbf4f51cffd71fc7a94
dillon <28228453+DillonAd@users.noreply.github.com>
2019-08-19 20:27:55 -0500

    Removing setter in Specification base class

--- EZSpecification/Specification.cs
+++ EZSpecification/Specification.cs
@@ -6,7 +6,7 @@ namespace EZSpecification
     public abstract class Specification<TEntity>
         where TEntity : class
     {
-        public abstract Expression<Func<TEntity, bool>> Expression { get; set; }
+        public abstract Expression<Func<TEntity, bool>> Expression { get; }
 
         public static implicit operator Func<TEntity, bool>(Specification<TEntity> specification) =>
             specification.Expression.Compile();
